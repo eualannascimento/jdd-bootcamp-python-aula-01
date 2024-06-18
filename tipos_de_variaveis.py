1349d740cb95fc34945e1afe0a62ac098fd00532
Alan Nascimento <eualannascimento@gmail.com>
2024-06-17 22:52:29 -0300

    adicionando anotações sobre a linguagem

---
+++ tipos_de_variaveis.py
@@ -0,0 +1,17 @@
+# Em python tudo é objeto, em python tudo é estrutura de dados
+# As variáveis são case sensitive
+
+## Tipos primitivos em python 
+3 # Inteiros: int
+3.5 # Números de ponto flutuante: float
+"Alan" # Strings: str
+True # Booleanos: bool
+
+## Tipos não primitivos
+# Listas: list
+# Tuplas: tuple
+# Dicionários: dict
+
+## Utilidades
+# Se você colocar um "_" na frente de uma variável, ela será considerada uma "variável secreta"
+_secrets = 1234567890
