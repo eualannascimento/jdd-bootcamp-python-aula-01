c52d253b8304f80ad822d24728cb30cbc29c1e8b
Alan Nascimento <eualannascimento@gmail.com>
2024-06-17 22:51:24 -0300

    inserindo primeiros comandos com a linguagem

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+# Testando os primeiros comandos do Python
+
+# print() - imprime algo no terminal
+print("Hello World!") 
+print(3)
+print(3 + 4)
+print("Oi" + " " + "Turma")
+print(True)
+
+# input() - pede pro usuário algum dado para entrada
+print("Oi" + " " +  input("Digite seu nome: "))
+
+# len() - conta a quantidade de caracteres de uma string
+print(len("Alan")) # Irá retornar 4
+
+# type() - imprime o tipo do que você desejar verificar
+print(type(3)) # Irá retornar int
