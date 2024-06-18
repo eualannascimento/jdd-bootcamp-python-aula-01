c27322e01e799397306b2374b6674cc2db4c1290
Alan Nascimento <eualannascimento@gmail.com>
2024-06-17 22:52:55 -0300

    inserindo resultados dos exercícios em aula

---
+++ exercicios.py
@@ -0,0 +1,8 @@
+# Exercício 01 - Crie um programa onde o usuário digita seu nome e o programa retorna a quantidade de caracteres do nome
+print(len(input("Digite seu nome: ")))
+
+# Exercício 02 e 03 - Crie um programa onde você vai digitar dois valores e ele deve retornar sua soma | Refatore usando variáveis
+valor01 = int(input("Digite o primeiro valor: "))
+valor02 = int(input("Digite o segundo valor: "))
+resultado = str(valor01 + valor02)
+print("O resultado da soma dos dois valores é de: " + resultado)
